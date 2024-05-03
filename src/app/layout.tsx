12ebf077a273f6ba4c27a1884e03a8004d448553
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-05-04 01:50:08 +0530

    Basic header added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <main className={inter.className}>{children}</main>
+      <body className=" bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+          <div className="lg:flex lg:justify-between lg:gap-4">
+            <Header />
+            <main className={inter.className}>{children}</main>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }

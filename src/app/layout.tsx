f8631c7b51157b466e25765c1b85e4301098044d
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-05-05 01:42:15 +0530

    root layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className=" bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
-        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-          <div className="lg:flex lg:justify-between lg:gap-4">
-            <Header />
-            <main className={inter.className}>{children}</main>
-          </div>
-        </div>
+        <main className={inter.className}>{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Header from "@/components/Header";
+import About from "@/pages/About";
+import Experience from "@/pages/Experience";
+import Project from "@/pages/Project";
+
 export default function Home() {
   return (
-    <main>
-      <h1>hello from main page</h1>
-    </main>
+    <>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <Header />
+          <main className="pt-24 lg:w-1/2 lg:py-24">
+            <About />
+            <Experience />
+            <Project />
+          </main>
+        </div>
+      </div>
+    </>
   );
 }

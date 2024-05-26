1f2b51966f7c87ed108cf8d59e4393e9f2862160
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-05-26 23:59:16 +0530

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className=" bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
-        <Cursor />
         <main className={inter.className}>{children}</main>
       </body>
     </html>
